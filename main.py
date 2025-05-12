4254644d74228345b21d90a8444ee09002409880
ashjackson82 <ashjackson82@gmail.com>
2025-05-12 13:31:10 -0400

    counts_words

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("greetings boots")
+def get_book_text(filepath): #get book text
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    from stats import count_book_words
+    num_words = count_book_words(text)
+    print(f"{num_words} words found in the document")
+
+main()
